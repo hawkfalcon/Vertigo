89f7a3919fcb2acd8a458e004e3a969069569a9d
hawkfalcon <hawkfalcon1@gmail.com>
2012-11-13 21:51:12 -0800

    Updated for 1.2 and 1.3

--- src/me/hawkfalcon/Vertigo/Vertigo.java
+++ src/me/hawkfalcon/Vertigo/Vertigo.java
@@ -22,7 +22,7 @@ public class Vertigo extends JavaPlugin implements Listener{
 
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
 		Player p = (Player) sender;
-		if ((p.hasPermission("Vertigo.use")) && (cmd.getName().equalsIgnoreCase("Vertigo"))) {
+		if ((p.hasPermission("Vertigo.use")) && (cmd.getName().equalsIgnoreCase("vertigo"))) {
 			if (args.length == 0) {
 				sender.sendMessage(ChatColor.RED + "You have a strange sense of vertigo");
 				p.removePotionEffect(PotionEffectType.SLOW);
@@ -32,10 +32,15 @@ public class Vertigo extends JavaPlugin implements Listener{
 				if (args[0].equalsIgnoreCase("stop")) {
 					p.removePotionEffect(PotionEffectType.SLOW);
 				}
+				if (args[0].equalsIgnoreCase("all") && ((p.hasPermission("vertigo.all")))) {
+					for (Player player : getServer().getOnlinePlayers()) {
+						player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 20000000, 15));					
+					}
+				}
 			}
 			if (args.length == 2) {
-				if (args[0].equalsIgnoreCase("player") && ((p.hasPermission("Vertigo.others")))) {
-	                if (getServer().getPlayer(args[1]) != null) {
+				if (args[0].equalsIgnoreCase("player") && ((p.hasPermission("vertigo.others")))) {
+					if (getServer().getPlayer(args[1]) != null) {
 						Player target = getServer().getPlayerExact(args[1]);
 						target.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 20000000, 15));
 						target.sendMessage(ChatColor.RED + "You have a strange sense of vertigo");
@@ -43,11 +48,19 @@ public class Vertigo extends JavaPlugin implements Listener{
 					else {
 						sender.sendMessage(ChatColor.RED + "You can only use this command on online players");
 					}
-	                
+
 				}
-				
+				if (args[0].equalsIgnoreCase("stop") && ((p.hasPermission("Vertigo.others")))) {
+					p.removePotionEffect(PotionEffectType.SLOW);
+					if (getServer().getPlayer(args[1]) != null) {
+						Player target = getServer().getPlayerExact(args[1]);
+						target.removePotionEffect(PotionEffectType.SLOW);
+					
+				}
+				}
+
 			}
-			
+
 		}
 
 		return true;
