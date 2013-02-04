0093c518d9464b228ba8d65bba80ca8cec60efc0
gomeow <davismariotti@gmail.com>
2013-02-04 07:31:35 -0800

    Do some things

--- src/me/hawkfalcon/Vertigo/Vertigo.java
+++ src/me/hawkfalcon/Vertigo/Vertigo.java
@@ -5,20 +5,12 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
 
-public class Vertigo extends JavaPlugin implements Listener{
-
-	public void onEnable() {
-		getServer().getPluginManager().registerEvents(this, this);
-	}
-
-	public void onDisable() {
-	}
+public class Vertigo extends JavaPlugin {
 
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
 		Player p = (Player) sender;
@@ -26,7 +18,7 @@ public class Vertigo extends JavaPlugin implements Listener{
 			if (args.length == 0) {
 				sender.sendMessage(ChatColor.RED + "You have a strange sense of vertigo");
 				p.removePotionEffect(PotionEffectType.SLOW);
-				p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 20000000, 15));
+				p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, Integer.MAX_VALUE, 15));
 			}
 			if (args.length == 1) {
 				if (args[0].equalsIgnoreCase("stop")) {
@@ -34,7 +26,7 @@ public class Vertigo extends JavaPlugin implements Listener{
 				}
 				if (args[0].equalsIgnoreCase("all") && ((p.hasPermission("vertigo.all")))) {
 					for (Player player : getServer().getOnlinePlayers()) {
-						player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 20000000, 15));					
+						player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, Integer.MAX_VALUE, 15));					
 					}
 				}
 			}
@@ -42,7 +34,7 @@ public class Vertigo extends JavaPlugin implements Listener{
 				if (args[0].equalsIgnoreCase("player") && ((p.hasPermission("vertigo.others")))) {
 					if (getServer().getPlayer(args[1]) != null) {
 						Player target = getServer().getPlayerExact(args[1]);
-						target.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 20000000, 15));
+						target.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, Integer.MAX_VALUE, 15));
 						target.sendMessage(ChatColor.RED + "You have a strange sense of vertigo");
 					}
 					else {
