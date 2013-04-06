da7883b13ccfd8941f9fd639efee96cb50b8e0b0
hawkfalcon <hawkfalcon1@gmail.com>
2013-04-05 18:34:01 -0700

    Fixed for 1.5, doesnt flip

--- src/me/hawkfalcon/Vertigo/Vertigo.java
+++ src/me/hawkfalcon/Vertigo/Vertigo.java
@@ -21,46 +21,49 @@ public class Vertigo extends JavaPlugin implements Listener{
 	}
 
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-		Player p = (Player) sender;
-		if ((p.hasPermission("Vertigo.use")) && (cmd.getName().equalsIgnoreCase("vertigo"))) {
+		String p = sender.getName();
+		Player pl = (Player)sender;
+		if ((pl.hasPermission("vertigo.use")) && (cmd.getName().equalsIgnoreCase("vertigo"))) {
 			if (args.length == 0) {
 				sender.sendMessage(ChatColor.RED + "You have a strange sense of vertigo");
-				p.removePotionEffect(PotionEffectType.SLOW);
-				p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 20000000, 15));
+				removePotions(p);
+				addPotions(p);
 			}
 			if (args.length == 1) {
-				if (args[0].equalsIgnoreCase("stop")) {
-					p.removePotionEffect(PotionEffectType.SLOW);
+				if (args[0].equalsIgnoreCase("stop") && ((pl.hasPermission("vertigo.stop"))))  {
+					removePotions(p);
+					pl.sendMessage(ChatColor.RED + "The vertigo subsides.");
 				}
-				if (args[0].equalsIgnoreCase("all") && ((p.hasPermission("vertigo.all")))) {
-					for (Player player : getServer().getOnlinePlayers()) {
-						player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 20000000, 15));					
+				if (args[0].equalsIgnoreCase("all") && ((pl.hasPermission("vertigo.all")))) {
+					for (Player pa : getServer().getOnlinePlayers()) {
+						addPotions(pa.getName());
+						pa.sendMessage(ChatColor.RED + "You have a strange sense of vertigo");
 					}
 				}
-				if (args[0].equalsIgnoreCase("stopall") && ((p.hasPermission("Vertigo.all")))) {
-					p.removePotionEffect(PotionEffectType.SLOW);
-					for (Player player : getServer().getOnlinePlayers()) {
-						player.removePotionEffect(PotionEffectType.SLOW);
+				if (args[0].equalsIgnoreCase("stopall") && ((pl.hasPermission("vertigo.all")))) {
+					for (Player pa : getServer().getOnlinePlayers()) {
+						removePotions(pa.getName());
+						pa.sendMessage(ChatColor.RED + "The vertigo subsides.");
 					}
 				}
 			}
 			if (args.length == 2) {
-				if (args[0].equalsIgnoreCase("player") && ((p.hasPermission("vertigo.others")))) {
-					if (getServer().getPlayer(args[1]) != null) {
-						Player target = getServer().getPlayerExact(args[1]);
-						target.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 20000000, 15));
-						target.sendMessage(ChatColor.RED + "You have a strange sense of vertigo");
+				if (args[0].equalsIgnoreCase("player") && ((pl.hasPermission("vertigo.others")))) {
+					if (getServer().getPlayerExact(args[1]) != null) {
+						String target = args[1];
+						addPotions(target);
+						getServer().getPlayer(target).sendMessage(ChatColor.RED + "You have a strange sense of vertigo");
 					}
 					else {
 						sender.sendMessage(ChatColor.RED + "You can only use this command on online players");
 					}
 
 				}
-				if (args[0].equalsIgnoreCase("stop") && ((p.hasPermission("Vertigo.others")))) {
-					p.removePotionEffect(PotionEffectType.SLOW);
+				if (args[0].equalsIgnoreCase("stop") && ((pl.hasPermission("vertigo.others")))) {
 					if (getServer().getPlayer(args[1]) != null) {
-						Player target = getServer().getPlayerExact(args[1]);
-						target.removePotionEffect(PotionEffectType.SLOW);
+						String target = args[1];
+						removePotions(target);
+						getServer().getPlayer(target).sendMessage(ChatColor.RED + "The vertigo subsides.");
 					}
 				}							
 				}
@@ -71,5 +74,15 @@ public class Vertigo extends JavaPlugin implements Listener{
 		
 
 		return true;
+	}
+	public void addPotions(String p){
+		getServer().getPlayer(p).addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 20000000, 15));
+		getServer().getPlayer(p).addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 20000000, 15));
+		getServer().getPlayer(p).addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 20000000, 15));
+	}
+	public void removePotions(String p){
+		getServer().getPlayer(p).removePotionEffect(PotionEffectType.SLOW);
+		getServer().getPlayer(p).removePotionEffect(PotionEffectType.CONFUSION);
+		getServer().getPlayer(p).removePotionEffect(PotionEffectType.BLINDNESS);
 	}
 }
